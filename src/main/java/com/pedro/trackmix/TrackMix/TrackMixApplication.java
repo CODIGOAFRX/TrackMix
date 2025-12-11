5c178bdba3d784f8d3676ff7526d9edb53f80903
Pedro Gómez <pedrojgomezperez@gmail.com>
2025-12-11 10:14:46 +0100

    Initial commit

---
+++ src/main/java/com/pedro/trackmix/TrackMix/TrackMixApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.trackmix.TrackMix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/trackmix/TrackMix/TrackMixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.trackmix.TrackMix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
